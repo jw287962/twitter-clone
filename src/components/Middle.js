93bd62b5f6fbe86e3e826119f854e7c73be7eca6
JasonPC <jasonwong287962@gmail.com>
2023-02-27 23:04:40 -0600

    made some default files for components

---
+++ src/components/Middle.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+import './css/Middle.css'
+const Middle = () => {
+
+  return (
+    <nav className="userNav">
+
+    </nav>
+  )
+}
+
+export default Middle; 
---
+++ src/components/Right.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+import './css/Right.css'
+const Right = () => {
+
+  return (
+    <nav className="searchNav">
+
+    </nav>
+  )
+}
+
+export default Right; 
